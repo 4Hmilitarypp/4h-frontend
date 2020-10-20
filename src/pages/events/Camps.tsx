d4bf1fbf90047a4342545daabd757fdf13a61b6a
Alex <alex@wendte.tech>
2020-10-19 22:51:31 -0500

    changes to Camp Dates

--- src/pages/events/Camps.tsx
+++ src/pages/events/Camps.tsx
@@ -231,7 +231,7 @@ const Camps: React.FC<RouteComponentProps> = ({ location }) => {
                     <CampDetailsWrapper>
                       <CampDetailsHeading>Age Range</CampDetailsHeading>
                       <CustomP>{camp.ageRange}</CustomP>
-                      <CampDetailsHeading>Camp dates</CampDetailsHeading>
+                      <CampDetailsHeading>Camp Dates</CampDetailsHeading>
                       {camp.dates.map(date => (
                         <CustomP key={camp.title + date.beginDate}>{`${format(
                           new Date(date.beginDate),

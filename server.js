5f9e6680ea09377e7be767f6deed9c0cf58cafea
Alex <alex@wendte.tech>
2021-03-24 09:27:33 -0500

    configures content security policy for google analytics

--- server.js
+++ server.js
@@ -5,11 +5,43 @@ const dotenv = require('dotenv')
 
 const app = express()
 dotenv.config()
-app.use(helmet())
+app.use(helmet({ contentSecurityPolicy: false }))
+// app.use(
+//   helmet.contentSecurityPolicy({
+//     directives: {
+//       ...helmet.contentSecurityPolicy.getDefaultDirectives(),
+//       'default-src': [
+//         "'self'",
+//         'https://www.google-analytics.com',
+//         'https://www.googletagmanager.com',
+//         'https://res.cloudinary.com',
+//         'https://cms.4-hmilitarypartnerships.org',
+//         "'unsafe-inline'",
+//         "'unsafe-eval'",
+//       ],
+//       'script-src': [
+//         "'self'",
+//         'https://www.google-analytics.com',
+//         'https://www.googletagmanager.com',
+//         'https://res.cloudinary.com',
+//         'https://cms.4-hmilitarypartnerships.org',
+//         "'unsafe-inline'",
+//         "'unsafe-eval'",
+//       ],
+//       'img-src': ["'self'", 'data:', 'www.googletagmanager.com', 'https://res.cloudinary.com'],
+//     },
+//   })
+// )
 
 app.get('/docker/docker-check', (_, res) => res.send('app is ready'))
 
 app.use(express.static(path.resolve(__dirname, './build'), { maxAge: '30d' }))
+// app.get('/service-worker.js', (_, res) => {
+//   res.sendFile(path.resolve(__dirname, './src/serviceWorker.js'))
+// })
+// app.get('/service-worker.js', (req, res) => {
+//   res.sendFile(path.resolve(__dirname, 'build', 'service-worker.js'))
+// })
 app.get('/*', (_, res) => {
   res.sendFile(path.join(__dirname, './build/index.html'), err => {
     if (err) {
